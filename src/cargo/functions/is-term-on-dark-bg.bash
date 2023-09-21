db8291a47e47c0dfd319d278531f441d7133960b
Jason Caldwell <jaswrks@o5p.me>
2023-09-20 22:33:43 -0400

    Project update. [p][robotic]

--- src/cargo/functions/is-term-on-dark-bg.bash
+++ src/cargo/functions/is-term-on-dark-bg.bash
@@ -12,7 +12,7 @@
 #
 # @return int `0` (true) if terminal has a dark background color.
 #
-# @review This needs more work in the future.
+# @someday This needs more work in the future.
 ##
 function :is-term-on-dark-bg() {
     if [[ -z "${COLORFGBG:-}" || "${COLORFGBG}" == '15;0' || "${COLORFGBG}" == '15;default;0' ]]; then
